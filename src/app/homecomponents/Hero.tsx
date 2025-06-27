3bb34230dab6e3546743eae2071b1fb433266995
Ivan Mulindo <chengoleivan@gmail.com>
2025-06-27 12:37:03 +0300

    Removed some boilerplate code

---
+++ src/app/homecomponents/Hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <section className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center">
+        <h1>Welcome to My Ticketing System</h1>
+        <p>The best ticketing system for all companies</p>
+    </section>
+  )
+}
